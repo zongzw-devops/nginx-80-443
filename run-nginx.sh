11a4e40633bc6338ee6446d2d971d25491fdbda6
Andrew ZONG <a.zong@f5.com>
2018-09-27 10:18:47 +0800

    enable 80 autoindex on; check user input.

--- run-nginx.sh
+++ run-nginx.sh
@@ -3,10 +3,11 @@
 cdir=`cd $(dirname $0); pwd`
 source $cdir/settings
 
-echo cleaning container if exists $container_name
-docker rm --force $container_name
-
 if [ -n "$webroot" ]; then 
+    if [ ! -d "$webroot" ]; then 
+        echo "ERROR: '$webroot' not exists"
+        exit 1
+    fi
     volume="-v $webroot:/usr/share/nginx/html"
     command="exec nginx -g 'daemon off;'"
 else
@@ -14,7 +15,10 @@ else
     command="echo Nginx Running: \$(hostname) > /usr/share/nginx/html/index.html && exec nginx -g 'daemon off;'"
 fi
 
-echo starting container $container_name ..
+echo "INFO: cleaning container if exists $container_name"
+docker rm --force $container_name
+
+echo "INFO: starting container $container_name .."
 docker run \
     --name $container_name \
     -itd \
